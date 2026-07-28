f3d135e324813d4c42f2040a7081079207696b68
EDITH <121100736+isurecan@users.noreply.github.com>
2026-07-29 02:28:25 +0530

    Add: spiral-matrix - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int colbegin=0;
+        int colend=matrix[0].length-1;
+        int rowbegin=0;
+        int rowend=matrix.length-1;
+        List<Integer> res=new ArrayList<>();
+        while(rowbegin<=rowend && colbegin<=colend){
+            //traverse right
+            for(int j=colbegin; j<=colend; j++){
+                res.add(matrix[rowbegin][j]);
+            }
+            rowbegin++;
+
+            //traverse down
+            for(int j=rowbegin; j<=rowend; j++){
+                res.add(matrix[j][colend]);
+            }
+            colend--;
+
+            //traverse left
+            if (rowbegin <= rowend)
+            for(int j=colend; j>=colbegin; j--){
+                res.add(matrix[rowend][j]);
+            }
+            rowend--;
+
+            //traverse up
+            if (colbegin <= colend)
+            for(int j=rowend; j>=rowbegin; j--){
+                res.add(matrix[j][colbegin]);
+            }
+            colbegin++;
+        }
+        return res;
+    }
+}
