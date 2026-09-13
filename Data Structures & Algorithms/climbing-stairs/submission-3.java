b7a1025b1ed88063d943c11d1d59c65234d30bc0
EDITH <121100736+isurecan@users.noreply.github.com>
2026-09-13 17:37:44 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] ways =new int[n+1];
+        if(n<2)
+        return 1;
+
+        ways[0]=1;
+        ways[1]=1;
+
+        for(int i =2; i<n+1; i++){
+            ways[i]=ways[i-1]+ways[i-2];
+        }
+
+        return ways[n];
+    }
+}
